84f2451c83ed13d9b1f3467b637b07a97ae11b1a
Владос Кирьянов <kiryanovvi@sushivesla.team>
2021-02-19 09:23:46 +0300

    Исправлено название класса IdGenerator

--- root/example/{{meta.domainLow}}/port/{{meta.domainUp}}IdGenerator.java
+++ root/example/{{meta.domainLow}}/port/{{meta.domainUp}}IdGenerator.java
@@ -3,5 +3,5 @@ package {{meta.package}}.domain.{{meta.domainLow}}.port;
 import com.rcore.domain.commons.port.BaseIdGenerator;
 
 //В разных бд или системах могут быть принято разное отношение к формату ID. А бизнес правил это не должно интересовать 
-public interface {{meta.domainUp}}Generator<T> extends BaseIdGenerator<T> {
+public interface {{meta.domainUp}}IdGenerator<T> extends BaseIdGenerator<T> {
 }
