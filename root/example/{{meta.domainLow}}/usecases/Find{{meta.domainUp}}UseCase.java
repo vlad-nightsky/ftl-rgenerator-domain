73a32b25854ec03d33d5624a6436f47f2be78dc2
Владос Кирьянов <kiryanovvi@sushivesla.team>
2021-05-01 12:09:34 +0300

    Добавил в генератор юзкейс поиска

---
+++ root/example/{{meta.domainLow}}/usecases/Find{{meta.domainUp}}UseCase.java
@@ -0,0 +1,16 @@
+package {{meta.package}}.domain.{{meta.domainLow}}.usecases;
+
+import com.rcore.domain.commons.usecase.AbstractFindWithFiltersUseCase;
+import {{meta.package}}.domain.{{meta.domainLow}}.entity.{{meta.domainUp}}Entity;
+import {{meta.package}}.domain.{{meta.domainLow}}.port.{{meta.domainUp}}Repository;
+import {{meta.package}}.domain.{{meta.domainLow}}.port.filters.{{meta.domainUp}}Filters;
+
+/**
+ * Поиск по коллекции
+ */
+public class Find{{meta.domainUp}}UseCase extends AbstractFindWithFiltersUseCase<{{meta.domainUp}}Entity, {{meta.domainUp}}Filters, {{meta.domainUp}}Repository> {
+
+    public Find{{meta.domainUp}}UseCase({{meta.domainUp}}Repository repository) {
+        super(repository);
+    }
+}
