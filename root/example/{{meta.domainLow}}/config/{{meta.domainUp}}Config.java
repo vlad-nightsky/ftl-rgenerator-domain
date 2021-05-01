7a2c74f3e029581e72b3f13189724799f0cf60d0
Владос Кирьянов <kiryanovvi@sushivesla.team>
2021-05-01 11:25:09 +0300

    Revert "Добавил в генератор Usecase для колекции"
    This reverts commit b54e4dda18af231cbde7047fabe1137d46fc32f1.

--- root/example/{{meta.domainLow}}/config/{{meta.domainUp}}Config.java
+++ root/example/{{meta.domainLow}}/config/{{meta.domainUp}}Config.java
@@ -8,7 +8,6 @@ import {{meta.package}}.domain.{{meta.domainLow}}.usecases.Create{{meta.domainUp
 import {{meta.package}}.domain.{{meta.domainLow}}.usecases.Delete{{meta.domainUp}}UseCase;
 import {{meta.package}}.domain.{{meta.domainLow}}.usecases.Find{{meta.domainUp}}ByIdUseCase;
 import {{meta.package}}.domain.{{meta.domainLow}}.usecases.Update{{meta.domainUp}}UseCase;
-import {{meta.package}}.domain.{{meta.domainLow}}.usecases.Find{{meta.domainUp}}UseCase;
 
 @Accessors(fluent = true)
 @Getter
@@ -17,13 +16,11 @@ public class {{meta.domainUp}}Config {
     private final Delete{{meta.domainUp}}UseCase delete{{meta.domainUp}}UseCase;
     private final Find{{meta.domainUp}}ByIdUseCase find{{meta.domainUp}}ByIdUseCase;
     private final Update{{meta.domainUp}}UseCase update{{meta.domainUp}}UseCase;
-    private final Find{{meta.domainUp}}UseCase find{{meta.domainUp}}UseCase;
 
     public {{meta.domainUp}}Config({{meta.domainUp}}Repository {{meta.domainLow}}Repository, {{meta.domainUp}}IdGenerator<?> {{meta.domainLow}}IdGenerator) {
         this.create{{meta.domainUp}}UseCase = new Create{{meta.domainUp}}UseCase({{meta.domainLow}}Repository, {{meta.domainLow}}IdGenerator);
         this.delete{{meta.domainUp}}UseCase = new Delete{{meta.domainUp}}UseCase({{meta.domainLow}}Repository);
         this.find{{meta.domainUp}}ByIdUseCase = new Find{{meta.domainUp}}ByIdUseCase({{meta.domainLow}}Repository);
         this.update{{meta.domainUp}}UseCase = new Update{{meta.domainUp}}UseCase({{meta.domainLow}}Repository);
-        this.find{{meta.domainUp}}UseCase = new Find{{meta.domainUp}}UseCase({{meta.domainLow}}Repository);
     }
 }
