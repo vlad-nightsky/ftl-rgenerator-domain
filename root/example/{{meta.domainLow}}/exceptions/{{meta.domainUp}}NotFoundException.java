3fc21cbdf235d9f9f81fa6d6861b0834984797ea
Владос Кирьянов <kiryanovvi@sushivesla.team>
2021-06-18 17:12:56 +0300

    refactor: увеличил отступ в конструкторе ошибки

--- root/example/{{meta.domainLow}}/exceptions/{{meta.domainUp}}NotFoundException.java
+++ root/example/{{meta.domainLow}}/exceptions/{{meta.domainUp}}NotFoundException.java
@@ -7,17 +7,17 @@ public class {{meta.domainUp}}NotFoundException extends NotFoundDomainException 
 
     public {{meta.domainUp}}NotFoundException() {
         super(
-            Domain.CATALOG.name(),
-            CategoryErrorReason.NOT_FOUND.name(),
-            "{{meta.domainUp}} not found"
+                Domain.CATALOG.name(),
+                CategoryErrorReason.NOT_FOUND.name(),
+                "{{meta.domainUp}} not found"
         );
     }
 
     public {{meta.domainUp}}NotFoundException(String id) {
         super(
-            Domain.CATALOG.name(),
-            CategoryErrorReason.NOT_FOUND.name(),
-            "{{meta.domainUp}} not found by ID: " + id
+                Domain.CATALOG.name(),
+                CategoryErrorReason.NOT_FOUND.name(),
+                "{{meta.domainUp}} not found by ID: " + id
         );
     }
 }
