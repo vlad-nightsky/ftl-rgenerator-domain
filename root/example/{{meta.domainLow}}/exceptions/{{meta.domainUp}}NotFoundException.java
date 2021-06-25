1066bd95f3b20c73b380bd9c21062cb94648efd0
Владос Кирьянов <kiryanovvi@sushivesla.team>
2021-06-25 15:50:45 +0300

    fix: исправил ошибку генерации ошибки

--- root/example/{{meta.domainLow}}/exceptions/{{meta.domainUp}}NotFoundException.java
+++ root/example/{{meta.domainLow}}/exceptions/{{meta.domainUp}}NotFoundException.java
@@ -6,16 +6,16 @@ public class {{meta.domainUp}}NotFoundException extends NotFoundDomainException 
 
     public {{meta.domainUp}}NotFoundException() {
         super(
-                Domain.CATALOG.name(),
-                CategoryErrorReason.NOT_FOUND.name(),
+                Domain.{{meta.domainCaps}.name(),
+                {{meta.domainUp}ErrorReason.NOT_FOUND.name(),
                 "{{meta.domainUp}} not found"
         );
     }
 
     public {{meta.domainUp}}NotFoundException(String id) {
         super(
-                Domain.CATALOG.name(),
-                CategoryErrorReason.NOT_FOUND.name(),
+                Domain.{{meta.domainCaps}.name(),
+                {{meta.domainUp}ErrorReason.NOT_FOUND.name(),
                 "{{meta.domainUp}} not found by ID: " + id
         );
     }
